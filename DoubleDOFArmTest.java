ce689f8db8e57f876f171e10b47ab095004d3779
queerboi <116319227+queerboi@users.noreply.github.com>
2023-10-15 20:34:23 -0700

    Arm 1.2

--- DoubleDOFArmTest.java
+++ DoubleDOFArmTest.java
@@ -4,31 +4,59 @@ import android.util.Pair;
 import android.util.Size;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+@TeleOp(name = "Two DoF Arm Test")
 public class DoubleDOFArmTest extends LinearOpMode {
+    // -- code helpers --
+    //just so the arm doesn't mistake 0 for an actual value
+    final double INFINITY = 9999999999.0;
+    //since I don't trust the computer on several trig calculations
+    final double TOLERANCE = Math.pow(10,-5);
+
+    //TODO 2.0: add potentiometers?
+
+    // -- arm specs --
+    //the unit doesn't matter as long as you are consistent with them.
+    //inches here.
+    final double R1 = 18;
+    final double R2 = 18;
+    final double ARM_HEIGHT = 12;
+
+    //remember to account for sprockets, pulleys, and gears!
+    final double TICKS_PER_MOTOR_ROTATION_1 = 1120;
+    final double TICKS_PER_MOTOR_ROTATION_2 = 1120;
+
+    //when you're not doing anything, please don't break the arm
+    final double STORAGE_ALPHA = -Math.acos(12/ R1);
+    final double STORAGE_BETA = Math.PI/2 - Math.asin(12/ R1);
+
+    // -- field specs --
+    //backdrop (and field) heights relative to tiles
+    final double FIELD_FLOOR = 0.0;
+    final double FIRST_LINE = 12.375;
+    final double SECOND_LINE = 19.0;
+    final double THIRD_LINE = 25.75;
+
+    //same as above, relative to first pivot point
+    final double GROUND_POSITION = FIELD_FLOOR - ARM_HEIGHT;
+    final double FIRST_POSITION = FIRST_LINE - ARM_HEIGHT;
+    final double SECOND_POSITION = SECOND_LINE - ARM_HEIGHT;
+    final double THIRD_POSITION = THIRD_LINE - ARM_HEIGHT;
+
+    // -- electronics setup: motors and webcams --
+    //motors
+    DcMotor topLeft, topRight, bottomLeft, bottomRight;
     DcMotor joint1, joint2;
 
-    final double r1 = 18;
-    final double r2 = 18;
-    double x,h;
-    double alpha,beta, alphaTicks, betaTicks;
-
-    final double STORAGE_ALPHA = -Math.acos(12/r1);
-    final double STORAGE_BETA = Math.PI/2 - Math.asin(12/r1);
-
-    final double GROUND_POSITION = -12;
-    final double FIRST_POSITION = 0;
-    final double SECOND_POSITION = 12;
-    final double THIRD_POSITION = 24;
-
-    final double TICKS_PER_ROTATION = 1120;
-
+    //set up vision for x
     AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
             .setDrawAxes(true)
             .setDrawCubeProjection(true)
@@ -42,26 +70,31 @@ public class DoubleDOFArmTest extends LinearOpMode {
             .setCameraResolution(new Size(640, 480)) // works well w/o impacting performance
             .build();
 
+    // -- not exactly code stuff
+    //variables to compute
+    double x,h;
+    double alpha,beta, alphaTicks, betaTicks;
+
     public void runOpMode() {
+        //drive motor setup
+        topLeft = hardwareMap.get(DcMotor.class, "top left");
+        topRight = hardwareMap.get(DcMotor.class, "top right");
+        bottomLeft = hardwareMap.get(DcMotor.class, "bottom left");
+        bottomRight = hardwareMap.get(DcMotor.class, "bottom right");
+
+        topLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        topRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //arm motor setup
+        joint1 = hardwareMap.get(DcMotor.class, "joint 1");
+        joint2 = hardwareMap.get(DcMotor.class, "joint 2");
+
         waitForStart();
         while (opModeIsActive()) {
-            if (gamepad2.atRest()) {
-                alpha = STORAGE_ALPHA;
-                beta = STORAGE_BETA;
-            } else {
-                h = setH();
-                x = setX();
-
-                alpha = calcAB(r1, r2, x, h).first;
-                beta = calcAB(r1, r2, x, h).second;
+            if (gamepad2.left_bumper) {
+                alignChassis();
             }
-            //convert alpha and beta to ticks
-            alphaTicks = TICKS_PER_ROTATION * alpha / (2 * Math.PI);
-            betaTicks = TICKS_PER_ROTATION * beta / (2 * Math.PI);
-
-            //move the motors with p control
-            joint1.setPower(0.01 * (alphaTicks - joint1.getCurrentPosition()));
-            joint2.setPower(0.01 * (betaTicks - joint2.getCurrentPosition()));
+            moveArm();
 
             //telemetry
             telemetry.addData("x: ", x);
@@ -74,6 +107,43 @@ public class DoubleDOFArmTest extends LinearOpMode {
         }
     }
 
+    //call moveArm() and alignChassis() in main loop
+    public void moveArm() {
+        if (gamepad2.atRest()) {
+            alpha = STORAGE_ALPHA;
+            beta = STORAGE_BETA;
+        } else {
+            h = setH();
+            x = setX();
+
+            alpha = calcAB(R1, R2, x, h).first;
+            beta = calcAB(R1, R2, x, h).second;
+        }
+        //convert alpha and beta to ticks
+        alphaTicks = TICKS_PER_MOTOR_ROTATION_1 * alpha / (2 * Math.PI);
+        betaTicks = TICKS_PER_MOTOR_ROTATION_2 * beta / (2 * Math.PI);
+
+        //move the motors with p control
+        joint1.setPower(0.01 * (alphaTicks - joint1.getCurrentPosition())); //TODO tune
+        joint2.setPower(0.01 * (betaTicks - joint2.getCurrentPosition())); //TODO tune
+    }
+
+    public void alignChassis() {
+        double toTurn = 0;
+        if(tagProcessor.getDetections().size() > 0){
+            AprilTagDetection tag = tagProcessor.getDetections().get(0);
+            toTurn = tag.ftcPose.bearing;
+            // side notes: bearing: left/right rotation to center
+        }
+        double power = toTurn * 0.01; //TODO tune
+        // CW if power +, check
+        topLeft.setPower(power);
+        topRight.setPower(-power);
+        bottomLeft.setPower(power);
+        bottomRight.setPower(-power);
+    }
+
+    //a ton of "helper" methods that do the actual heavy lifting
     public double setH() {
         if (gamepad2.a) {
             return GROUND_POSITION;
@@ -84,7 +154,7 @@ public class DoubleDOFArmTest extends LinearOpMode {
         } else if (gamepad2.y) {
             return THIRD_POSITION;
         } else {
-            return 0;
+            return INFINITY;
         }
     }
 
@@ -94,12 +164,12 @@ public class DoubleDOFArmTest extends LinearOpMode {
             return tag.ftcPose.x;
             // side notes: bearing: left/right rotation to center, elevation: up/down rotation to center
         }
-        return 0;
+        return INFINITY;
     }
 
     public boolean checkAB(double alpha, double beta) {
-        //might need to add tolerance to this
-        return Math.cos(alpha-beta) == (h*h + x*x - r1*r1 - r2*r2)/(2*r1*r2);
+        //TODO add tolerance
+        return Math.abs(Math.cos(alpha-beta) - (h*h + x*x - R1 * R1 - R2 * R2)/(2* R1 * R2)) <= TOLERANCE;
     }
 
     public Pair<Double, Double> calcAB(double r1, double r2, double x, double h) {
@@ -129,8 +199,8 @@ public class DoubleDOFArmTest extends LinearOpMode {
             return new Pair<>(alphaNeg, betaNeg);
         }
         else {
-            //no solutions
-            return new Pair<>(0.0,0.0);
+            //no solutions --> keep in storage position
+            return new Pair<>(STORAGE_ALPHA,STORAGE_BETA);
         }
     }
 }
